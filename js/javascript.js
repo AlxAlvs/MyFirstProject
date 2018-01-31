9de74026fd676ce4e20c98b80210ddf8e9a8507f
alxalvs <alxalvs0121@gmail.com>
2018-01-31 14:04:19 -0200

    js externo

---
+++ js/javascript.js
@@ -0,0 +1,34 @@
+
+ $(window).on('load', function () {
+      $("#loading").css("display", "none");
+     $("#conteudo").css("display", "block");
+        $('[data-toggle="tooltip"]').tooltip();
+    })
+
+    var slideIndex = 1;
+    showSlides(slideIndex);
+
+    
+    function adiciona_slide(n) {
+    showSlides(slideIndex += n);
+    }
+    
+    function slide_atual(n) {
+    showSlides(slideIndex = n);
+    }
+    function showSlides(n) {
+      var i;
+      var slides = document.getElementsByClassName("slide");
+      var dots = document.getElementsByClassName("dot");
+      if (n > slides.length) {slideIndex = 1} 
+      if (n < 1) {slideIndex = slides.length}
+      for (i = 0; i < slides.length; i++) {
+          slides[i].style.display = "none"; 
+      }
+      for (i = 0; i < dots.length; i++) {
+          dots[i].className = dots[i].className.replace("active", "");
+      }
+      slides[slideIndex-1].style.display = "block"; 
+      dots[slideIndex-1].className += " active";
+    }
+
